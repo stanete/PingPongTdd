0615640ac2681c5304567c400081d08df1eeb533
David Stanete <davidstanete@gmail.com>
2019-06-04 16:46:02 +0200

    Movie serializer genre

--- src/videoclub/movies/serializers.py
+++ src/videoclub/movies/serializers.py
@@ -8,13 +8,19 @@ class MovieSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
-        fields = ('title', 'price', 'score',)
+        fields = ('title', 'price', 'score', 'genre',)
         read_only_fields = ('score',)
+        extra_kwargs = {
+            'genre': {'source': 'get_genre_display'},
+        }
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
 
         if not flag.is_active('show_ratings'):
             del representation['score']
+
+        if not representation['genre']:
+            del representation['genre']
 
         return representation
