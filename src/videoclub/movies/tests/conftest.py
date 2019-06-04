007d703cd91b33724d9bb0568e1adab2fa34bfb6
David Stanete <davidstanete@gmail.com>
2019-06-04 16:45:55 +0200

    Test movie serializer genre

--- src/videoclub/movies/tests/conftest.py
+++ src/videoclub/movies/tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from videoclub.movies import choices
 from videoclub.movies.models import Movie
 
 
@@ -11,3 +12,9 @@ def movie():
 @pytest.fixture
 def movie_with_score():
     return Movie.objects.create(title='Die Hard', price=9, score=10)
+
+
+@pytest.fixture
+def movie_with_genre():
+    return Movie.objects.create(
+        title='Fast and Furious', score=8, price=7, genre=choices.ACTION)
--- src/videoclub/movies/tests/test_serializers.py
+++ src/videoclub/movies/tests/test_serializers.py
@@ -6,22 +6,33 @@ from ..serializers import MovieSerializer
 @pytest.mark.django_db
 class TestMovieSerializer:
 
-    def test_serializes_valid_movie_with_genre(self, movie_with_score):
-        serializer = MovieSerializer(movie_with_score)
+    def test_serializes_valid_movie_with_genre(self, movie_with_genre):
+        assert movie_with_genre.score == 8
+
+        serializer = MovieSerializer(movie_with_genre)
 
         assert serializer.data == {
-            'title': 'Die Hard',
-            'price': '9.00',
+            'title': 'Fast and Furious',
+            'price': '7.00',
+            'genre': 'Action',
         }
 
     @pytest.mark.usefixtures('flag_show_ratings_active')
-    def test_serializes_score_when_flag_is_active(self, movie_with_score):
-        assert movie_with_score.score == 10
+    def test_serializes_score_when_flag_is_active(self, movie_with_genre):
+        assert movie_with_genre.score == 8
 
-        serializer = MovieSerializer(movie_with_score)
+        serializer = MovieSerializer(movie_with_genre)
 
         assert serializer.data == {
-            'title': 'Die Hard',
-            'price': '9.00',
-            'score': 10,
+            'title': 'Fast and Furious',
+            'price': '7.00',
+            'score': 8,
+            'genre': 'Action',
         }
+
+    def test_does_not_serialize_genre_when_movie_genre_is_null(self, movie):
+        assert movie.genre is None
+
+        serializer = MovieSerializer(movie)
+
+        assert 'genre' not in serializer.data
