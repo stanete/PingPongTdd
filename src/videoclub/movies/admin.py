82184adb0516bbcb317056ce50975b298932da3a
David Stanete <davidstanete@gmail.com>
2019-06-04 16:33:10 +0200

    Test movie admin configuration

--- src/videoclub/movies/admin.py
+++ src/videoclub/movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.admin import register
+
+from videoclub.movies.models import Movie
+
+
+@register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    pass
---
+++ src/videoclub/movies/tests/test_admin.py
@@ -0,0 +1,25 @@
+import pytest
+from django.contrib.admin import AdminSite
+
+from videoclub.movies.admin import MovieAdmin
+from videoclub.movies.models import Movie
+
+
+class TestMovieAdmin:
+
+    @pytest.fixture
+    def admin_site(self):
+        return AdminSite()
+
+    @pytest.fixture
+    def movie_admin(self, admin_site):
+        return MovieAdmin(Movie, admin_site)
+
+    def test_fields_is_set(self, movie_admin):
+        assert movie_admin.fields == ('title', 'price', 'score',)
+
+    def test_list_display_is_set(self, movie_admin):
+        assert movie_admin.list_display == ('title', 'score',)
+
+    def test_readonly_fields_is_set(self, movie_admin):
+        assert movie_admin.readonly_fields == ('score',)
