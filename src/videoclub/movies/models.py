2ca786f7ff0331c2b080757543d7584d9bfe64ed
David Stanete <davidstanete@gmail.com>
2019-06-04 14:49:40 +0200

    Add score field with default value to movie model

--- src/videoclub/movies/models.py
+++ src/videoclub/movies/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from rest_framework.compat import MaxValueValidator, MinValueValidator
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=256)
     price = models.DecimalField(decimal_places=2, max_digits=5)
+    score = models.IntegerField(
+        default=0, validators=[MinValueValidator(0), MaxValueValidator(10)])
