d9f61db72d4971b6b2ca76530030320df36aa4ac
David Stanete <davidstanete@gmail.com>
2019-06-04 14:49:30 +0200

    Test movie score field has default value

---
+++ src/videoclub/movies/tests/test_models.py
@@ -0,0 +1,18 @@
+import pytest
+
+from videoclub.movies.models import Movie
+
+
+@pytest.mark.django_db
+class TestMovie:
+
+    def test_creates_a_movie_when_passing_title_and_price(self):
+        Movie.objects.create(title='Titanic', price='3.95')
+
+        assert Movie.objects.count() == 1
+
+    def test_creates_a_movie_with_default_score_to_zero(self):
+        Movie.objects.create(title='Titanic', price='3.95')
+
+        movie = Movie.objects.get(title='Titanic', price='3.95')
+        assert movie.score == 0
