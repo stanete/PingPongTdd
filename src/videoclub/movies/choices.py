4d572e349023cb3e66a0298468ed2e4003fc08f8
David Stanete <davidstanete@gmail.com>
2019-06-04 16:36:13 +0200

    Add genre field to movie model

---
+++ src/videoclub/movies/choices.py
@@ -0,0 +1,11 @@
+ACTION = 1
+COMEDY = 2
+FANTASY = 3
+DRAMA = 4
+
+GENRE_CHOICES = (
+    (ACTION, 'Action'),
+    (COMEDY, 'Comedy'),
+    (FANTASY, 'Fantasy'),
+    (DRAMA, 'Drama'),
+)
--- src/videoclub/movies/models.py
+++ src/videoclub/movies/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from rest_framework.compat import MaxValueValidator, MinValueValidator
 
+from videoclub.movies import choices
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=256)
     price = models.DecimalField(decimal_places=2, max_digits=5)
     score = models.IntegerField(
         default=0, validators=[MinValueValidator(0), MaxValueValidator(10)])
+    genre = models.PositiveSmallIntegerField(null=True, choices=choices.GENRE_CHOICES)
