d9937605b4162c4c1ab19faba91afe652929e71a
David Stanete <davidstanete@gmail.com>
2019-06-04 16:31:10 +0200

    GET movie with score when feature flag is active

--- src/videoclub/movies/serializers.py
+++ src/videoclub/movies/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from videoclub.flags import flag
 from videoclub.movies.models import Movie
 
 
@@ -7,4 +8,12 @@ class MovieSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
-        fields = ('title', 'price',)
+        fields = ('title', 'price', 'score',)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        if not flag.is_active('show_ratings'):
+            del representation['score']
+
+        return representation
