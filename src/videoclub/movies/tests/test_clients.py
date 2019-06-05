7497fdda2a6e8396101ab6821da5f490d1ce8008
Daniel Ruiz <druizca@mercadona.es>
2019-06-05 11:32:56 +0200

    Test client exception when OMDB is unavailable

--- src/videoclub/movies/tests/test_clients.py
+++ src/videoclub/movies/tests/test_clients.py
@@ -1,6 +1,9 @@
 from decimal import Decimal
 
-from ..clients import OMDBClient
+import pytest
+import requests
+
+from ..clients import OMDBClient, Unavailable
 
 
 class TestOMDBClient:
@@ -14,3 +17,15 @@ class TestOMDBClient:
         rating = OMDBClient().get_rating_for_movie(title='Die Hard')
 
         assert rating == Decimal('8.2')
+
+    def test_raises_unavailable_when_connection_error(self):
+        with pytest.raises(Unavailable):
+            OMDBClient().get_rating_for_movie(title='Die Hard')
+
+    def test_raises_unavailable_when_request_timeout(self, responses):
+        request_url = 'http://www.omdbapi.com/?apikey=my-api-key&t=Die+Hard'
+        responses.add(responses.GET, request_url, match_querystring=True,
+                      body=requests.Timeout())
+
+        with pytest.raises(Unavailable):
+            OMDBClient().get_rating_for_movie(title='Die Hard')
