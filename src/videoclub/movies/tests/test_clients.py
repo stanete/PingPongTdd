586ea1c6a9827e5c7210d40c29ded822a1ac37dd
Daniel Ruiz <druizca@mercadona.es>
2019-06-05 11:23:37 +0200

    Test client rating as decimal

--- src/videoclub/movies/tests/test_clients.py
+++ src/videoclub/movies/tests/test_clients.py
@@ -1,9 +1,11 @@
+from decimal import Decimal
+
 from ..clients import OMDBClient
 
 
 class TestOMDBClient:
 
-    def test_returns_movie_rating_when_passing_valid_title(self, responses):
+    def test_returns_movie_rating_as_decimal_when_passing_valid_title(self, responses):
         OMDBClient.API_KEY = 'my-api-key'
         request_url = 'http://www.omdbapi.com/?apikey=my-api-key&t=Die+Hard'
         responses.add(responses.GET, request_url, match_querystring=True, status=200,
@@ -11,4 +13,4 @@ class TestOMDBClient:
 
         rating = OMDBClient().get_rating_for_movie(title='Die Hard')
 
-        assert rating == '8.2'
+        assert rating == Decimal('8.2')
